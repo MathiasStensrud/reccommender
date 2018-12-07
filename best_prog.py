8edabb319a0cf3f1e498083e49fccd08cf699788
mathiasstensrud <matt.stens@gmail.com>
2018-12-07 14:55:18 -0700

    agaaaain

--- best_prog.py
+++ best_prog.py
@@ -9,7 +9,8 @@ from sklearn.metrics import mean_absolute_error as mae
 spark = SparkSession.builder.getOrCreate()
 
 
-def recommend_user(userId, recs, movies):
+def recommend_user(userId, recs):
+    movie_df=pd.read_csv('data/movies/movies.csv')
     temp=recs.loc[recs['userId'] == userId]
     r=np.array(temp.recommendations.all())[:,0]
     temp=[]
@@ -18,10 +19,19 @@ def recommend_user(userId, recs, movies):
     r=[]
     return temp
 
-spark_df=spark.createDataFrame(pd.read_csv('data/movies/ratings.csv'))
+# spark_df=spark.createDataFrame(pd.read_csv('data/movies/ratings.csv'))
 movie_df=pd.read_csv('data/movies/movies.csv')
-spark_df=spark_df.drop('timestamp')
-spark_df=spark_df.filter(spark_df.userId!=468)
+df=pd.read_csv('data/movies/ratings.csv')
+df.drop('timestamp',axis=1, inplace=True)
+spark_df=spark.createDataFrame(df)
+# spark_df=spark_df.filter(spark_df.userId!=468)
+pandas_count = spark_df.groupby("movieId").count().toPandas()
+pandas_mean = spark_df.groupby("movieId").mean('rating').toPandas()
+# temp=recs.loc[recs['userId'] == userId]
+bad=pandas_mean.loc[pandas_mean['avg(rating)']==5]
+spark_df=spark.createDataFrame(df[~df['movieId'].isin(bad.movieId)])
+
+
 train, test = spark_df.randomSplit([0.8, 0.2])
 als_model = ALS(
     itemCol='movieId',
@@ -44,5 +54,6 @@ print('rmse: ',rmse,' mae: ',m)
 spark_df=rec.transform(spark_df)
 df=spark_df.toPandas().fillna(train_df.prediction.mean(), inplace=True)
 recs=rec.recommendForAllUsers(5).toPandas()
+recs.to_csv('static/recs.csv')
 
-print(recommend_user(50, recs, movie_df))
+print(recommend_user(50, recs))
